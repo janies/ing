648fee0b4fc3ee4bed5e5ec56aff6b4ec1a93dfd
John Zachary <johnzachary2@gmail.com>
2016-09-08 18:28:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ing
+
+[![GoReportCard](http://goreportcard.com/badge/elastic/beats)](http://goreportcard.com/report/elastic/beats)
+
+A packet processor and metadata collected for industrial control system (ISC) networks.
+
+## Dependencies
+
+## Build
+
+## Usage
---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION=0.0.0
+
+go build -ldflags \
+    "-X main.version=${VERSION} -X main.buildTime=`date -u +%Y-%m-%d_%I:%M:%S%p` -X main.gitHash=`git rev-parse HEAD`"
+
+echo ""
+echo "     ************************************************"
+echo "        Don't forget to update the VERSION string."
+echo "        The current VERSION is ${VERSION}."
+echo "        See https://semver.org for rules."
+echo "     ************************************************"
+echo ""
